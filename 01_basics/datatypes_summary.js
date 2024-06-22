9c1605c3150b7aceb8d38b1c9d42f6418bf05f99
PratimaP23 <156713745+PratimaP23@users.noreply.github.com>
2024-06-22 07:07:44 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String , Number , Boolean , null(empty) , undefined , symbol , BigInt\
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId );
+
+ const bigNumber = 345678293648937583n
+
+
+// Reference (Non primitive)
+
+// Array , Objects , Functions  // datatypes of non primitive = object
+
+const heros = ["saktiman","naagraj","doga"];
+let myObj ={
+        name : "hitesh",
+        age :  22,
+
+}
+
+const myFunction = function(){
+      console.log("Hello World");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
