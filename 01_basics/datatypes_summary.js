3e6bb73191bccb78b542aa5e5aa2834a9df11100
PratimaP23 <156713745+PratimaP23@users.noreply.github.com>
2024-06-22 17:14:27 +0530

    Update datatypes_summary.js

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -37,6 +37,9 @@ console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive) , Heap(Non-Primitive)
 
 let myYoutubename = "hiteshchoudarydotcom"
 
